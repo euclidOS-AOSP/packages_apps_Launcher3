d0b08ea9b2ac8e00f88a1c917806fc7f7bf9056e
Pranav Vashi <neobuddy89@gmail.com>
2021-02-27 10:00:38 +0530

    Launcher3: Add icon and icon text size customizations
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- src/com/android/launcher3/LauncherPrefs.kt
+++ src/com/android/launcher3/LauncherPrefs.kt
@@ -243,6 +243,8 @@ constructor(@ApplicationContext private val encryptedContext: Context) {
         @JvmField val PROMISE_ICON_IDS = nonRestorableItem(InstallSessionHelper.PROMISE_ICON_IDS, "")
         @JvmField val WORKSPACE_LOCK = backedUpItem("pref_workspace_lock", false)
         @JvmField val ALLAPPS_THEMED_ICONS = backedUpItem("pref_allapps_themed_icons", false)
+        @JvmField val FONT_SIZE = backedUpItem("pref_custom_font_size", 100)
+        @JvmField val ICON_SIZE = backedUpItem("pref_custom_icon_size", 100)
         @JvmField val SHOW_DESKTOP_LABELS = backedUpItem("pref_desktop_show_labels", true)
         @JvmField val SHOW_DRAWER_LABELS = backedUpItem("pref_drawer_show_labels", true)
         @JvmField val WORK_EDU_STEP = backedUpItem("showed_work_profile_edu", 0)
