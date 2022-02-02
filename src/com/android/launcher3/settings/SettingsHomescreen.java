bb910fe01ac44e6476f0eda4cd0b648549ded8ed
Pranav Vashi <neobuddy89@gmail.com>
2022-02-03 03:24:11 +0530

    Launcher3: Add toggle disable hotseat searchbar
    Squashed:
    
        From: Pranav Vashi <neobuddy89@gmail.com>
        Date: Fri, 30 Dec 2022 02:49:41 +0530
        Subject: Launcher3: Fix taskbar crash without QSB
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Change-Id: I606ead4bad62ab8dff836f3fd0a4c2a6c4d786f9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -111,7 +111,8 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (LauncherPrefs.SHOW_HOTSEAT_BG.getSharedPrefKey().equals(key) ||
-                LauncherPrefs.HOTSEAT_OPACITY.getSharedPrefKey().equals(key)) {
+                LauncherPrefs.HOTSEAT_OPACITY.getSharedPrefKey().equals(key) ||
+                LauncherPrefs.DOCK_SEARCH.getSharedPrefKey().equals(key)) {
             LauncherAppState.INSTANCE.executeIfCreated(app -> app.setNeedsRestart());
         }
     }
@@ -171,6 +172,7 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
         private static final String KEY_MINUS_ONE = "pref_enable_minus_one";
 
         private Preference mShowGoogleAppPref;
+        private Preference mShowGoogleBarPref;
 
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -192,6 +194,8 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
             PreferenceScreen screen = getPreferenceScreen();
 
             mShowGoogleAppPref = screen.findPreference(KEY_MINUS_ONE);
+            mShowGoogleBarPref = screen.findPreference(LauncherPrefs.DOCK_SEARCH.getSharedPrefKey());
+
             updateIsGoogleAppEnabled();
 
             // If the target preference is not in the current preference screen, find the parent
@@ -276,6 +280,9 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
         private void updateIsGoogleAppEnabled() {
             if (mShowGoogleAppPref != null) {
                 mShowGoogleAppPref.setEnabled(Utilities.isGSAEnabled(getContext()));
+            }
+            if (mShowGoogleBarPref != null) {
+                mShowGoogleBarPref.setEnabled(Utilities.isGSAEnabled(getContext()));
             }
         }
 
